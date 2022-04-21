534d489481fcbc8c28dfd078d8b229d64d923a7c
RudhyDev <rudhy.pereira@gmail.com>
2022-04-21 01:09:01 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots : ['<rootDir>/src'],
+  testEnvironment : 'node',
+  tranform: {
+    '.+\\.ts$':'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1'
+  }
+}
