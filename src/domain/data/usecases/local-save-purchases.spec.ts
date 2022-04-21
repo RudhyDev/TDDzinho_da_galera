40272ff5f48ad4aa3f8f9bfd301e68c0872ad1ec
RudhyDev <rudhy.pereira@gmail.com>
2022-04-21 11:54:58 -0300

    move SUT creation to a factory helper method

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -13,22 +13,36 @@ interface CacheStore {
 class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0;
 
-    delete(): void{
-      this.deleteCallsCount++
+    delete(): void {
+        this.deleteCallsCount++;
     }
 }
+
+type SutTypes = {
+    sut: LocalSavePurchases;
+    cacheStore: CacheStoreSpy;
+};
+
+// Isso é uma Factory para instanciar as classes necessárias para testar o SUT
+const makeSut = (): SutTypes => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore);
+
+    return {
+        cacheStore,
+        sut,
+    };
+};
 
 // sut => nomenclatura para o construtor da classe que está sendo testada (manguinhos)
 describe("LocalSavePurchases", () => {
     test("Should not delete cache on sut.init", () => {
-        const cacheStore = new CacheStoreSpy();
-        new LocalSavePurchases(cacheStore);
+        const { cacheStore } = makeSut();
         expect(cacheStore.deleteCallsCount).toBe(0);
     });
 
     test("Should delete old cache on sut.delete", async () => {
-        const cacheStore = new CacheStoreSpy();
-        const sut = new LocalSavePurchases(cacheStore);
+        const { cacheStore, sut } = makeSut();
         await sut.save();
         expect(cacheStore.deleteCallsCount).toBe(1);
     });
