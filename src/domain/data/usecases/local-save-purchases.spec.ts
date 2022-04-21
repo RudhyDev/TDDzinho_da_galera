57d8a8a8b1ed9728a41569b77b635ce85eb1b2c3
RudhyDev <rudhy.pereira@gmail.com>
2022-04-21 11:43:31 -0300

    Ensure LocalSavePurchases deletes old cache on save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,21 +1,35 @@
 class LocalSavePurchases {
-  constructor(private readonly cacheStore: CacheStore) {}
+    constructor(private readonly cacheStore: CacheStore) {}
+
+    async save(): Promise<void> {
+        this.cacheStore.delete();
+    }
 }
 
 interface CacheStore {
-  
+    delete: () => void;
 }
 
 class CacheStoreSpy implements CacheStore {
-  deleteCallsCount = 0
+    deleteCallsCount = 0;
+
+    delete(): void{
+      this.deleteCallsCount++
+    }
 }
 
-
 // sut => nomenclatura para o construtor da classe que está sendo testada (manguinhos)
-describe('LocalSavePurchases', () => {
-  test('Should not delete cache on sut.init', () => {
-    const cacheStore = new CacheStoreSpy();
-    new LocalSavePurchases(cacheStore);
-    expect(cacheStore.deleteCallsCount).toBe(0)
-  })
-})
+describe("LocalSavePurchases", () => {
+    test("Should not delete cache on sut.init", () => {
+        const cacheStore = new CacheStoreSpy();
+        new LocalSavePurchases(cacheStore);
+        expect(cacheStore.deleteCallsCount).toBe(0);
+    });
+
+    test("Should delete old cache on sut.delete", async () => {
+        const cacheStore = new CacheStoreSpy();
+        const sut = new LocalSavePurchases(cacheStore);
+        await sut.save();
+        expect(cacheStore.deleteCallsCount).toBe(1);
+    });
+});
